411ef46062579ed4175eaa7cdac5f93d41fd7471
icecream0910 <taein2370@naver.com>
2025-06-07 23:43:24 +0900

    fix: EBS FM 즐겨찾기 중복 표시 이슈

--- pages/components/favoriteStationList.js
+++ pages/components/favoriteStationList.js
@@ -1,19 +1,21 @@
 import IonIcon from '@reacticons/ionicons';
 import { useRecoilState } from 'recoil';
 import { playerData, favoritesData } from '../../states/states';
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import toast from 'react-hot-toast';
 
+const allRadioStations = require('/public/radioStations.json');
+const uniqueRadioStations = Array.from(new Map(allRadioStations.map(station => [station.title, station])).values());
+
 export default function FavoriteStationList() {
-    const radioData = require('/public/radioStations.json');
     const [player, setPlayer] = useRecoilState(playerData);
     const [favorites, setFavorites] = useRecoilState(favoritesData);
-    const [actualFavorites, setActualFavorites] = useState([])
 
-    const toggleFavorites = (title) => {
-        if (favorites.includes(title)) {
-            setFavorites(favorites.filter(favorite => favorite !== title));
-            toast.dismiss();
+    const toggleFavorites = (stationTitle) => {
+        toast.dismiss();
+
+        if (favorites.includes(stationTitle)) {
+            setFavorites(favorites.filter(title => title !== stationTitle));
             toast('자주 듣는 목록에서 제거했어요.', {
                 icon: '🗑️',
                 duration: 2000,
@@ -28,33 +30,36 @@ export default function FavoriteStationList() {
                 }
             });
         } else {
-            setFavorites([...favorites, title]);
-            toast.dismiss();
+            setFavorites(prevFavorites => [...prevFavorites, stationTitle]);
         }
-    }
+    };
 
-    useEffect(() => {
-        setActualFavorites(favorites);
+    const favoriteStationsToDisplay = useMemo(() => {
+        return uniqueRadioStations.filter(station => favorites.includes(station.title));
     }, [favorites]);
 
-
-    return (<>
-        {actualFavorites.length === 0 ? (
+    if (favoriteStationsToDisplay.length === 0) {
+        return (
             <p style={{ opacity: .8, textAlign: 'center', marginTop: '5em', fontSize: 'var(--content-font-size)' }}>
                 자주 듣는 스테이션이 하나도 없네요.<br />
                 스테이션 옆 <IonIcon name='heart-outline' /> 버튼을 눌러 추가해보세요.
             </p>
-        ) : (
-            radioData.filter(radio => actualFavorites.includes(radio.title)).map((radio, index) => (
-                <div className={`station-item ${player.title === radio.title ? 'active' : ''}`} key={index}>
-                    <span style={{ width: '90%' }} onClick={() => setPlayer(radio)}>{radio.title}&nbsp;
-                        {player.title === radio.title ? <span className='badge'>재생중</span> : ''}</span>
-                    <button onClick={() => toggleFavorites(radio.title)}>
-                        {actualFavorites.includes(radio.title) ? <IonIcon name='heart' /> : <IonIcon name='heart-outline' />}
+        );
+    }
+
+    return (
+        <>
+            {favoriteStationsToDisplay.map((station) => (
+                <div className={`station-item ${player.title === station.title ? 'active' : ''}`} key={station.title}>
+                    <span style={{ width: '90%' }} onClick={() => setPlayer(station)}>
+                        {station.title}&nbsp;
+                        {player.title === station.title ? <span className='badge'>재생중</span> : ''}
+                    </span>
+                    <button onClick={() => toggleFavorites(station.title)}>
+                        {favorites.includes(station.title) ? <IonIcon name='heart' /> : <IonIcon name='heart-outline' />}
                     </button>
                 </div>
-            ))
-        )}
-
-    </>)
+            ))}
+        </>
+    );
 }
